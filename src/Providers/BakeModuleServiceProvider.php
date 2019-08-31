0c3e00c9b9df4d5c5b6928e1781d43a55ff323dd
flavianohonorato <flaviano.honorato@gmail.com>
2019-08-31 11:24:26 -0300

    fix import service provider

--- src/Providers/BakeModuleServiceProvider.php
+++ src/Providers/BakeModuleServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\ServiceProvider;
 
 class BakeModuleServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function register()
     {
         if ($this->app->runningInConsole()) {
             $this->commands([
